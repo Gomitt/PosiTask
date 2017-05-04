e3115d0f6437a2fad24c57ce71018e3260b582be
Paz Bunis <paz.bunis@weather.com>
2017-05-04 17:12:12 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for, request, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+@app.route('/<param>')
+def main(param='hike'):
+    return render_template('app.html', param = positize(param))
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/do')
+def do():
+    return render_template('do.html')
+
+def positize(task):
+    return 'Posi' + task.title()
