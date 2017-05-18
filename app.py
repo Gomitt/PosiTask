c0ec13df5562a557ba8e510cf1768eb1a8b566bb
mottig <mottigold@gmail.com>
2017-05-18 16:20:54 +0300

    added Task class & playground.py

--- app.py
+++ app.py
@@ -23,19 +23,6 @@ def create():
         return render_template('create.html')
     else:
         pass
-#
-# @app.route('/add', methods=['POST'])
-# def add_entry():
-#     if not session.get('logged_in'):
-#         abort(401)
-#     db = get_db()
-#     db.execute('insert into entries (title, text) values (?, ?)',
-#                [request.form['title'], request.form['text']])
-#     db.commit()
-#     flash('New entry was successfully posted')
-#     return redirect(url_for('show_entries'))
-#
-
 
 @app.route('/do')
 def do():
