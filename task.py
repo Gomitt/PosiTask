cd8fce4d0a5698fcdaa76be1c084313b158fd9a6
gitLouis <plusel@gmail.com>
2017-05-18 11:21:35 +0300

    task file

---
+++ task.py
@@ -0,0 +1,16 @@
+# import geocoder?
+
+class Class:
+    def __init__(self):
+        self.title = []
+        self.description = []
+        self.creator = []
+        self.value = 1
+        self.creationDate = []
+        self.inOutOnline = "in" #in / out / online
+        self.type = "kids" # kids sport & body posiTask socializing friendships and family adventures waste my time read and learn be good enviroment music and art
+        self.location = "geocode text"
+
+    def get(self):
+        return
+
