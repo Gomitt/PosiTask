a0cbd2a95cc7fda8083b71284fb838607e7fb81c
mottig <mottigold@gmail.com>
2017-05-19 01:25:20 +0300

    try to avoid caching

--- app.py
+++ app.py
@@ -44,3 +44,14 @@ def do(task_id=None):
     print(task)
     return render_template('do.html', task=task)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
