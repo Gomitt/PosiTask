6deefb23b653b7bfe9ca134730490e6d16905fa8
gitlouis <plusel@gmail.com>
2017-05-18 22:52:31 +0300

    posiTaskExamples.csv

--- playground.py
+++ playground.py
@@ -1,5 +1,19 @@
 from task_db import TaskDB
+import csv
 
+filename = 'db/posiTask examples - tasks.csv'
 db = TaskDB()
-db.insert_task(title='hi', desc='bye', creator='sdf', in_out_online=0, task_type=1, location="dsfsadf", value=2)
-print()
+with open(filename, newline='') as f:
+    reader = csv.reader(f)
+    next(reader)
+    next(reader)
+    for row in reader:
+        row = [s.replace("\n","<br>") for s in row]
+        print(row)
+        db.insert_task(row[1],row[2],row[4],row[7],row[3],row[6],row[5],1)
+
+for task_type, vals in db.get_tasks_dict().items():
+    for t in vals:
+        print(t.get_json())
+
+db.save('db/db1.pickle')
